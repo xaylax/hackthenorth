684519d0484a5b54dfaa44b3baae8e16e4099a03
Ioana Bruj <t-iobruj@microsoft.com>
2019-09-14 20:43:45 -0400

    Merge, environment page

--- app/src/main/java/com/pd/htn/EnvironmentFragment.kt
+++ app/src/main/java/com/pd/htn/EnvironmentFragment.kt
@@ -1,13 +1,20 @@
 package com.pd.htn
 
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.pd.htn.vm.EnvironmentViewModel
+import kotlinx.android.synthetic.main.environment.*
 
 class EnvironmentFragment : Fragment() {
 
@@ -20,6 +27,22 @@ class EnvironmentFragment : Fragment() {
     ): View? {
         vm = ViewModelProvider(activity as AppCompatActivity).get(EnvironmentViewModel::class.java)
 
+
+//        val howButton: Button = view!!.findViewById(R.id.how_button)
+//        howButton.setOnClickListener{
+//            val popup = PopupWindow(view,LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+//            val textView: TextView = view!!.findViewById(R.id.what_carbon_footprint)
+//            textView.visibility = View.VISIBLE
+//            val buttonPopUp = view!!.findViewById<Button>(R.id.how_button)
+//            buttonPopUp.setOnClickListener{
+//                popup.dismiss()
+//            }
+//
+//            TransitionManager.beginDelayedTransition(environment)
+//            popup.showAtLocation(environment,Gravity.CENTER,0,0)
+//        }
         return inflater.inflate(R.layout.environment, container, false)
+
+
     }
 }
